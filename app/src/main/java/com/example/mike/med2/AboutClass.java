797277c0f100027f3ad9a16bfc74b39b72ac2689
mike <miklekreslavskiy@gmail.com>
2019-03-03 20:13:18 +0300

    Animation

--- app/src/main/java/com/example/mike/med2/AboutClass.java
+++ app/src/main/java/com/example/mike/med2/AboutClass.java
@@ -3,37 +3,45 @@ package com.example.mike.med2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
-public class AboutClass extends Activity {
+public class AboutClass extends AppCompatActivity {
     protected Button butgame;
+    boolean flag=false;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.nerv_system);
-        butgame=(Button) findViewById(R.id.button3);
+        final TranslateAnimation animation = new TranslateAnimation(-500, 300, 50, 50);
+        animation.setDuration(1000);
+        animation.setFillAfter(true);
 
-        butgame.setOnClickListener(new View.OnClickListener() {
+        final Button btnTranslate = (Button)findViewById(R.id.translate);
+        Button button=(Button)findViewById(R.id.body);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AboutClass.this, Game.class);
-                startActivity(intent);
+                if (flag == false) {
+                    btnTranslate.startAnimation(animation);
+                    flag = true;
+
+                }
             }
         });
     }
 
 
-    public void onClick(View view) {
-        Intent intent = new Intent(AboutClass.this, Brain.class);
-        startActivity(intent);
-
-    }
-
-    }
+}
 
 
 
